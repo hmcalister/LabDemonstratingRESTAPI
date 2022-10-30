c91840cd61d6cb688b69a3384b7b14b340af07f8
Hayden McAlister <haydenmcalister49@gmail.com>
2022-10-30 18:14:27 +1300

    Add start of  main module

---
+++ main/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"hmcalister/api"
+	"hmcalister/database"
+	"hmcalister/models"
+	"log"
+	"os"
+)
+
+func checkFatalError(err error) {
+	if err != nil {
+		log.Fatalln("ERROR: ", err)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Println("ERROR: ", err)
+	}
+}
+
+func insertTestData() {
+	err := database.CreateDatabase("database.db")
+	checkFatalError(err)
+
+	newStudents := []models.Student{
+		{
+			StudentCode: "hayden000",
+			FullName:    "Hayden McAlister",
+		},
+		{
+			StudentCode: "alice123",
+			FullName:    "Alice",
+		},
+		{
+			StudentCode: "bob456",
+			FullName:    "Bob",
+		},
+	}
+
+	for _, student := range newStudents {
+		database.CreateStudent(student)
+	}
+}
+
+func main() {
+	os.Remove("database.db")
+	insertTestData()
+	api.SetupAPI()
+}

2f969c8fce4efd534ce39d0aa28f4b1dbc7a410b
Hayden McAlister <haydenmcalister49@gmail.com>
2022-10-30 18:13:27 +1300

    Add models for basis of schema

---
+++ models/models.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `json:"Username" gorm:"primaryKey;autoIncrement:false;unique:true"`
+	Password string `json:"Password"`
+}
+
+type Student struct {
+	gorm.Model
+	StudentCode string `json:"StudentCode" gorm:"primaryKey;column:StudentCode;unique:true"`
+	FullName    string `json:"FullName"`
+}
+
+type Lab struct {
+	gorm.Model
+	ID          int    `json:"LabID" gorm:"primaryKey"`
+	LabName     string `json:"LabName"`
+	Description string `json:"Description"`
+	Points      int    `json:"Points"`
+}
+
+type LabCompletion struct {
+	gorm.Model
+	StudentCode string    `json:"StudentCode" gorm:"column:StudentCode;primaryKey;autoIncrement:false;"`
+	Student     Student   `gorm:"foreignKey:StudentCode"`
+	LabID       int       `json:"LabID" gorm:"column:LabID;primaryKey;autoIncrement:false"`
+	Lab         Lab       `gorm:"foreignKey:LabID"`
+	Timestamp   time.Time `json:"Timestamp"`
+}

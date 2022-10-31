19b74174b6def646348a2e28e353ddbc7bfafb8d
Hayden McAlister <haydenmcalister49@gmail.com>
2022-11-01 12:19:39 +1300

    Start adding command line arguement parsing using Flag

--- main/main.go
+++ main/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"hmcalister/api"
 	"hmcalister/database"
 	"hmcalister/models"
+	"io"
 	"log"
 	"os"
 
+	"github.com/gin-gonic/gin"
 )
 
 func checkFatalError(err error) {
@@ -21,8 +25,9 @@ func checkError(err error) {
 	}
 }
 
-func insertTestData() {
-	err := database.CreateDatabase("database.db")
+func insertTestData(databaseFilepath string) {
+	os.Remove(databaseFilepath)
+	err := database.CreateDatabase(databaseFilepath)
 	checkFatalError(err)
 
 	newStudents := []models.Student{
@@ -46,9 +51,21 @@ func insertTestData() {
 }
 
 func main() {
-	os.Remove("database.db")
-	insertTestData()
-	api.SetupAPI()
+	var err error
+	var routerLogFile io.Writer
+
+	databaseFilepath := flag.String("database", "database.db", "Database file to use. Defaults to 'database.db'.")
+	routerLogFilepath := flag.String("routerLog", "", "File to store router logs. If not set, print router logs to STDOUT.")
+	portNumber := flag.Int("port", 80, "Port to serve webserver and API on. Defaults to 80.")
+	flag.Parse()
+	if *routerLogFilepath == "" {
+		routerLogFile = os.Stdout
+	} else {
+		routerLogFile, err = os.OpenFile(*routerLogFilepath, os.O_RDWR|os.O_CREATE, 0644)
+		checkFatalError(err)
+	}
+
+	insertTestData(*databaseFilepath)
 
 	gin.DefaultWriter = routerLogFile
 	router := gin.Default()
